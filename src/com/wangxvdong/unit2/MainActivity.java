e883812d17c912e95f62214d9e0e8223b4007d78
XD <XD@WXD-Workstation>
2014-04-15 10:51:23 +0800

    Revert "项目初始文件"
    This reverts commit 75726048707952c301496fc2205bec96070b5c60.

--- src/com/wangxvdong/unit2/MainActivity.java
+++ src/com/wangxvdong/unit2/MainActivity.java
@@ -4,17 +4,17 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -66,12 +66,6 @@ public class MainActivity extends Activity {
 
     public void btn1_onClick(View v) {
         showDialog(0);
-        NotificationManager nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        Notification notif=new Notification(R.drawable.ic_launcher,"First Activity Started!",System.currentTimeMillis());
-        notif.setLatestEventInfo(this, "AndroidApplication1", "Start Activity 1", PendingIntent.getActivity(this, 0, new Intent(this,NotificationView.class), 0));
-        notif.vibrate=new long[]{100,250,100,250};
-        nm.notify(index, notif);
-       // nm.cancel(index);
     }
 
     public void onClick2(final View v) {
